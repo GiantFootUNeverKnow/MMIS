54f551e0d3a40a3bca7a0393351d22d8d5b89090
GiantFootUNeverKnow <zxsnbnb@gmail.com>
2016-11-11 03:08:18 -0600

    A word searching script for E1

---
+++ Experiment/E1/clip.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
+
+expectations = [0] * 19
+variances = [0] * 19
+directory = os.getcwd() + '/result'
+for filename in os.listdir(directory):
+    number = int(filename[6:])
+    a = open(directory + '/' + filename, 'rb')
+    lines = a.readlines()
+    if lines:
+        firstline = lines[-2]
+        secondline = lines[-1]
+        expectation = float(firstline.rsplit(None, 1)[-1])
+        variance = float(secondline.rsplit(None, 1)[-1])
+        expectations[number - 1] = expectation
+        variances[number - 1] = variance
+    a.close()
+
+print expectations
+print variances
+plt.plot(expectation)
