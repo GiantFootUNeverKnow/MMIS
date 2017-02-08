1ea071dab520e19cc2ffe1454a2fa78e72ee7e3c
GiantFootUNeverKnow <zxsnbnb@gmail.com>
2017-02-07 21:21:19 -0600

    E9 done

--- Experiment/E9/run2.sh
+++ Experiment/E9/run2.sh
@@ -4,6 +4,6 @@ for i in {2..3}
 do
     for j in {1..12}
     do
-        ./main.py --experiment Experiment/E9/config$i --jb Experiment/E9/E9.$j/jobs --repeat 100 > Experiment/E9/E9.$j/result$i
+        ./main.py --experiment Experiment/E9/config$i --jb Experiment/E9/E9.$j/jobs > Experiment/E9/E9.$j/result$i
     done
 done
